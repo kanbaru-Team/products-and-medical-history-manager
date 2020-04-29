ec0402a4f1f9ceaf9cfe03d03ff1e6aae400ccff
Marco <macroxx120@gmail.com>
2020-04-29 18:24:51 -0500

    created MainView.fxml, VeterinaryMenu and VeterinaryGUI. Now the graphic intreface is started bus just show the menu for the veterinary. there are also changes in the Main class made to make the GUI works

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,10 +1,33 @@
 package ui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import model.Veterinary;
 
+public class Main extends Application {
+
+	Veterinary veterinary = new Veterinary();
+	VeterinaryGUI veterinaryGUI = new VeterinaryGUI(veterinary);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch();
 
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainView.fxml"));
+		fxmlLoader.setController(veterinaryGUI);
+		
+		Parent root = fxmlLoader.load();
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("veterinary");
+		primaryStage.show();
+		
 	}
 
 }
---
+++ src/ui/VeterinaryGUI.java
@@ -0,0 +1,65 @@
+package ui;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.BorderPane;
+import model.Veterinary;
+
+public class VeterinaryGUI {
+
+    @FXML
+    private BorderPane mainPane;//panel principal
+    
+    Veterinary veterinary;
+    
+    public VeterinaryGUI(Veterinary v) {
+		veterinary = v;
+	}
+    
+    //metodo para mostrar el menu de la tienda en la pantalla principal
+    @FXML
+    public void showMedicalHistoryMenu(ActionEvent event) throws IOException {
+    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("VeterinaryMenu.fxml"));
+    	fxmlLoader.setController(this);
+    	
+    	Parent medicalHistoryPane = fxmlLoader.load();
+    	mainPane.getChildren().clear();
+    	mainPane.setCenter(medicalHistoryPane);
+    }
+
+    //metodo para mostrar el menu del historial medico en la pantalla principal
+    @FXML
+    public void showStoreMenu(ActionEvent event) {
+
+    }
+    
+    //metodo para mostrar el menu de buscar un paciente 
+    @FXML
+    public void showLookPatientMenu(ActionEvent event) {
+
+    }
+    
+    //metodo para mostrar el menu de añadir un nuevo paciente
+    @FXML
+    public void showAddPatientMenu(ActionEvent event) {
+
+    }
+    
+    //metodo para mostrar el menu de ver los pacientes hospitalizados
+    @FXML
+    public void showHospitalizedPatientsMenu(ActionEvent event) {
+
+    }
+	
+    //metodo para mostrar el menu de ver los pacientes registrados
+    @FXML
+    void showRegisteredPatientsMenu(ActionEvent event) {
+
+    }
+	
+}
