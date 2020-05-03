320c6133760bad2e29907fd2555a7c0277abd4ea
Marco <macroxx120@gmail.com>
2020-05-03 09:00:41 -0500

    now the user is able to create products

--- src/ui/UpdateMedicalHistoryGUI.java
+++ src/ui/UpdateMedicalHistoryGUI.java
@@ -1,5 +1,17 @@
 package ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 public class UpdateMedicalHistoryGUI {
+
+    @FXML
+    private TextArea newInformation;
+
+    @FXML
+    void updateHistory(ActionEvent event) {
+
+    }
 
 }
